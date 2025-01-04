0b0ed1774b698bbf5704123004223f4ae304a182
Julino Segunda Dídimo <ddimojulino@gmail.com>
2025-01-04 15:20:01 -0300

    Melhorei o toggle do modal

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,20 @@
 const btn = document.querySelector('.btn-menu')
+const closeBtn = document.querySelector('.close')
+const modal = document.querySelector('.modal')
 
-btn.addEventListener('click', function(){
+btn.addEventListener('click', function () {
     console.log('Click funcionando')
+    if (!modal.classList.contains('hidden')) {
+        modal.classList.add('hidden')
+    }
+})
+closeBtn.addEventListener('click', function () {
+    console.log('Fechar modlal')
+    if (modal.classList.contains('hidden')) {
+        modal.classList.remove('hidden')
+        console.log('remover hidden')
+    } else {
+        modal.classList.add('hidden')
+        console.log('adicionar hidden')
+    }
 })
